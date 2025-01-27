094291d8672fe568c572939be3b01d7626e695f6
CedeJostin <jostin.cedeno@ulatina.net>
2025-01-27 16:46:57 -0600

    Actualizar el manejo de solicitudes en la API para incluir CORS y mejorar el registro de mensajes

--- qtrustai/app/api/receive-messages/route.js
+++ qtrustai/app/api/receive-messages/route.js
@@ -2,12 +2,14 @@ export async function POST(request) {
   const { message } = await request.json();
   const { parte1, parte2, parte3 } = message;
 
-  // Procesa los datos recibidos
-  console.log('Parte 1:', parte1);
-  console.log('Parte 2:', parte2);
-  console.log('Parte 3:', parte3);
+  // Log de la solicitud
+  console.log('Solicitud recibida:', message);
 
   return new Response(JSON.stringify({ status: 'success' }), {
-    headers: { 'Content-Type': 'application/json' },
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*', // Permite solicitudes de cualquier dominio
+      'Access-Control-Allow-Methods': 'POST', // Asegúrate de permitir el método POST
+    },
   });
 }
